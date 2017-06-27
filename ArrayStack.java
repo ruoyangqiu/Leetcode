915cd93743810eea2a0c90fc1b95288e3ebb0c1b
ruoyangqiu <qiur2@uw.edu>
2017-06-26 21:26:57 -0700

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,19 +1,24 @@
+// Use Array to implent Stack.
+
 import java.util.EmptyStackException;
 
 public class ArrayStack implements DStack {
-	private double[] stackArray;
-	private int size;
-	private static final int Initial_Size = 10;
+	private double[] stackArray; // Store the element of stack
+	private int size;	      // the size of the stack
+	private static final int Initial_Size = 10; //the initial size of the array when stack initialized
 	
 	public ArrayStack(){
 		stackArray = new double[Initial_Size];
 		size = 0;
 	}
 	
+	
 	public boolean isEmpty(){
 		return size == 0;
 	}
 	
+	// inseart the new data into the stack
+	// if the element reach the maximum size of the arra, double the array size
 	public void push(double d){
 		if(stackArray.length == size){
 			double[] temp = new double[2*stackArray.length];
@@ -23,6 +28,8 @@ public class ArrayStack implements DStack {
 		size ++;
 	}
 	
+	// remove the last added element, and return it
+	// threw EmptyStackExpection if the stack is empty 
 	public double pop(){
 		if(size == 0){
 			throw new EmptyStackException();
@@ -31,6 +38,7 @@ public class ArrayStack implements DStack {
 		return stackArray[size];
 	}
 	
+	// return the last added element
 	public double peek(){
 		if(size == 0){
 			throw new EmptyStackException();
