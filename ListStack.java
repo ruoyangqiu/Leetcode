c6ef17ffc7a84541e5baa154cdeac59c5d35414d
ruoyangqiu <qiur2@uw.edu>
2017-06-26 21:23:51 -0700

    Update ListStack.java

--- ListStack.java
+++ ListStack.java
@@ -1,18 +1,23 @@
+// Use LinkedList to implent Stack.
+
 import java.util.EmptyStackException;
 
 public class ListStack implements DStack {
-	private int size;
-	private ListStackNode node;
+	private int size; // I am not sure if the size is necessary here? 
+	private ListStackNode node; // Store the data of stack
 	
+	// Constructor
 	public ListStack(){
 		node = null;
 		size = 0;
 	}
 	
+	// Boolean if the stack is empty
 	public boolean isEmpty(){
 		return node == null;
 	}
 	
+	//inseart the new data into the stack
 	public void push(double d){
 		ListStackNode last = new ListStackNode(d);
 		last.next = node;
@@ -20,6 +25,8 @@ public class ListStack implements DStack {
 		size ++;
 	}
 	
+	// remove the last added element, and return it
+	// threw EmptyStackExpection if the stack is empty
 	public double pop(){
 		if(this.isEmpty()){
 			throw new EmptyStackException();
@@ -30,6 +37,7 @@ public class ListStack implements DStack {
 		return data;
 	}
 	
+	// return the last added element
 	public double peek(){
 		if(this.isEmpty()){
 			throw new EmptyStackException();
